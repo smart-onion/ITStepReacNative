a331f6955ae97400f45f163d0aa93ee5ac99ca1e
smart-onion <sanyamart13@gmail.com>
2026-02-26 23:24:28 +0200

    register validation

--- src/pages/auth/Auth.tsx
+++ src/pages/auth/Auth.tsx
@@ -1,69 +1,68 @@
-import { Text, TextInput, TouchableOpacity, View } from 'react-native';
+import { Text, TouchableOpacity, View } from 'react-native';
 import AuthStyle from './css/AuthStyle';
-import { useContext, useEffect, useState } from 'react';
+import { useContext, useState } from 'react';
 import { textColor } from '../../features/values/colors';
 import AppContext from '../../features/context/AppContext';
+import SignUpView from './ui/SignUpView';
+import SignInView from './ui/SignInView';
+import { ButtonTypes, FirmButton } from '../../features/ui/button/FirmButton';
 
 
 export default function Auth() {
-    const [login, setLogin] = useState("user");
-    const [password, setPassword] = useState("123");
-    const [isFormValid, setFormValid] = useState(false);
-    const {user, setUser} = useContext(AppContext);
+    const {user} = useContext(AppContext);
+    const [pageMode, setPageMode] = useState("SignIn");
 
-    useEffect(() => {
-        setFormValid(login.length > 2 && password.length > 2);
-    }, [login, password]);
 
-    const signInClick = () => {
-        if(login == 'user' && password == '123') {
-            setUser({
-                name: 'User',
-                token: '123',
-                email: "user@local.lan",
-                phone: "+380637891653",
-                currency: "UAH"
-            });
+    return !!user 
+    ? <SignedView />
+    : <View style={AuthStyle.authContainer}>        
+        <PageSwitchWidget pageMode={pageMode} setPageMode={setPageMode}/>        
+        {pageMode == "SignIn"
+        ? <SignInView />
+        : <SignUpView setPageMode={setPageMode} />
         }
-    };
+    </View>;
+}
+
+function PageSwitchWidget({pageMode, setPageMode}:{pageMode:string, setPageMode:React.Dispatch<React.SetStateAction<string>>}) {
+    return <View style={AuthStyle.pageSwitch}>
+        <FirmButton title='Вхід' style={AuthStyle.pageSwitchButton}
+            buttonType={pageMode == "SignUp" ? ButtonTypes.primary : ButtonTypes.success}
+            action={() => setPageMode("SignIn")}/>
+        <FirmButton title='Реєстрація' style={AuthStyle.pageSwitchButton}
+            buttonType={pageMode == "SignUp" ? ButtonTypes.success : ButtonTypes.primary} 
+            action={() => setPageMode("SignUp")} />
+    </View>;
+}
+
+function SignedView() {
+    const {user, setUser} = useContext(AppContext);
 
     const signOutClick = () => {
         setUser(null);
     }
-
-    return !!user 
-    ? <View style={AuthStyle.authContainer}>
-        <View style={AuthStyle.userDetails}>
-            <Text style={AuthStyle.authRowText}>Вітання, {user.name}</Text>
-            <Text style={AuthStyle.authRowText}>Email: {user.email}</Text>
-            <Text style={AuthStyle.authRowText}>Phone number: {user.phone}</Text>
-            {user.currency && <Text style={AuthStyle.authRowText}>Your currency: {user.currency}</Text>}
+    return <View style={AuthStyle.authContainer}>
+        <View style={AuthStyle.authRow}>
+            <Text style={AuthStyle.authRowText}>Вітання, {user!.name}</Text>
+        </View>
+        <View style={AuthStyle.authRow}>
+            <Text style={AuthStyle.authRowText}>E-mail: {user!.email}</Text>
+        </View>
+        <View style={AuthStyle.authRow}>
+            <Text style={AuthStyle.authRowText}>Телефон: {user!.phone}</Text>
+        </View>
+        <View style={AuthStyle.authRow}>
+            <Text style={AuthStyle.authRowText}>Дата народження: {user!.birthdate.toDotted()}</Text>
+        </View>
+        <View style={AuthStyle.authRow}>
             <TouchableOpacity style={AuthStyle.authButton} onPress={signOutClick}>
                 <Text style={[AuthStyle.authButtonText, {color: textColor}]}>Вихід</Text>
             </TouchableOpacity>
         </View>
-    </View>
-
-    : <View style={AuthStyle.authContainer}>
-        <View style={AuthStyle.authRow}>
-            <Text style={AuthStyle.authRowText}>Логін</Text>
-            <TextInput style={AuthStyle.authRowInput} value={login} onChangeText={setLogin} />
-        </View>
-
-        <View style={AuthStyle.authRow}>
-            <Text style={AuthStyle.authRowText}>Пароль</Text>
-            <TextInput secureTextEntry={true} style={AuthStyle.authRowInput} value={password} onChangeText={setPassword} />
-        </View>
-
-        <TouchableOpacity style={AuthStyle.authButton} onPress={isFormValid ? signInClick : undefined}>
-            <Text style={[AuthStyle.authButtonText, {color: isFormValid ? textColor : "#777"}]}>Вхід</Text>
-        </TouchableOpacity>
-
     </View>;
 }
 /*
-Д.З. Розширити дані, що характеризують користувача:
-додати номер телефону, E-mail, інші дані (за профілем застосунку)
-вивести відповідні дані на сторінці-профілі,
-до звіту з ДЗ додати скріншот
+Д.З. Стилізувати сторінку профіля користувача.
+Реалізувати режим редагування даних з реактивною валідацією
+(деактивація кнопки збереження у разі некоректних даних)
 */
